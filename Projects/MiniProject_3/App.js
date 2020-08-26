464dbb8a598ed21adac9b4830ccf2e35f6417c7b
Kowshik407 <aitha.kowshik3@gmail.com>
2020-08-26 19:34:04 +0530

    Changed

---
+++ Projects/MiniProject_3/App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+
+function App() {
+    return (
+        <div className='App'>
+            <Mini />
+            <Project3 />
+        </div>
+    );
+}
+
+export default App;
